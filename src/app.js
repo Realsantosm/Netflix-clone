8d9e516f65d1cf814102bc64680fdcd8a9474dca
Santos <realsantosm@gmail.com>
2021-12-25 20:41:48 +0530

    feat : added pages

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Home, Browse, SignIn, SignUp } from './pages';
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
+import * as ROUTES from './constants/routes';
 
 
 
 export default function App() {
   return (
-    <>
+    <Router>
     <JumbotronContainer />
     <FaqsContainer />
     <FooterContainer />
-    </>
+    </Router>
     );
 }
 
 
+ 
