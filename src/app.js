1a05c726cbad0c409ef6fa29898e3d8035276203
Santos <realsantosm@gmail.com>
2021-12-26 13:11:20 +0530

    feat : Added

--- src/app.js
+++ src/app.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
-import { FaqsContainer } from './containers/faqs';
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
+export function App() {
+  const { user } = useAuthListener();
 
-
-export default function App() {
   return (
     <Router>
-    <JumbotronContainer />
-    <FaqsContainer />
-    <FooterContainer />
+       <Switch>
+         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+           <SignIn />
+         </IsUserRedirect>
+         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+           <SignUp />
+         </IsUserRedirect>
+         <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+           <Browse />
+         </ProtectedRoute>
+         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+           <Home />
+         </IsUserRedirect>
+       </Switch>
     </Router>
-    );
+  );
 }
-
-
- 
